1cef268530bb64cfbf3c2860b0b21b2a006168a7
Segun Ojo <olusegunkd2017@gmail.com>
2025-09-18 17:09:58 +0100

    first part home done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,18 @@ import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
+  const SEARCH_FILTERS = [
+    { tag: "Funny", color: "#F1A62D" },
+    { tag: "Wholesome", color: "#C7B9FF" },
+    { tag: "Challenging", color: "#FFEBA6" },
+    { tag: "Inspiring", color: "#CFFF98" },
+  ];
+
   return (
     <div className="mt-[22px]">
-      <main className={`bg-[url("/assets/pattern-bg.svg")]`}>
-        <div className="flex flex-col items-center pt-[156px]">
-          <h1 className="italic text-[48px]/[24px] font-medium mb-[11px]">
+      <main className={`bg-[url("/assets/pattern-bg.svg")] bg-white rounded-[30px]`}>
+        <div className="flex flex-col items-center pt-[156px] pb-[65px]">
+          <h1 className="italic text-[48px]/[24px] font-medium mb-[15px]">
             The Search Wall 👀
           </h1>
           <p className="text-[#3C3C4399] font-normal text-[17px]/[22px]">
@@ -21,7 +28,21 @@ export default function Home() {
               width={20}
               height={20}
             />
-            <Input className="border w-full border-[#D0D5DD]  bg-white rounded-[90px] py-[14px] pl-[40px] pr-[16px]" />
+            <Input placeholder="Search dreamboard.." className="border w-full border-[#D0D5DD] font-serif  bg-white rounded-[90px] py-[14px] pl-[40px] pr-[16px]" />
+          </div>
+          <div className="flex items-center gap-3 mt-[27px] inter-font">
+            <p className="text-[13px]/[16px] font-normal">Filter Search:</p>
+            <div className=" gap-2 flex font-medium text-[13px]/[18px]">
+              {SEARCH_FILTERS.map(({tag, color}: {tag: string, color: string}) => (
+                <div className={`cursor-pointer hover:scale-105 transition-1000 py-[7px] px-4 rounded-[64px] bg-[#F1A62D]`} style={
+                  {
+                    backgroundColor: color
+                  }
+                } >
+                  {tag}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </main>
